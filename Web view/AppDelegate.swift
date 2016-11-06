d3ad7ca753e0a0e91c42b4f41d824fc1f789810a
Roberto Chiarella <Roberto@imac-di-roberto.local>
2016-11-06 23:26:02 +0100

    Initial Commit

---
+++ Web view/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Web view
+//
+//  Created by Roberto Chiarella on 06/11/16.
+//  Copyright © 2016 Roberto Chiarella. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Web view/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Web view
+//
+//  Created by Roberto Chiarella on 06/11/16.
+//  Copyright © 2016 Roberto Chiarella. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
